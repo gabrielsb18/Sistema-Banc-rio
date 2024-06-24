79f8af83811022addde6e32f98c18ace8b275b30
gabrielsb18 <gbmodelator@gmail.com>
2024-06-24 17:01:15 -0300

    Deposito da conta criado

--- src/index.js
+++ src/index.js
@@ -62,4 +62,22 @@ app.get("/statement/", verifyIFExistsAccountCPF,(request, response)=> {
     return response.json(customer.statement);
 });
 
+
+app.post("/deposit",verifyIFExistsAccountCPF,(request, response) => {
+    const {description, amount} = request.body;
+
+    const {customer} = request;
+
+    const statementOperation = {
+        description,
+        amount,
+        cretea_at: new Date(),
+        type: "credit"
+    }
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+})
+
 app.listen(3333);
